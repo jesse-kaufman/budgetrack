1408a62fbbd3c955e8dffee735ad6a0cbe71e25c
jesse-kaufman <jesse@jessekaufman.com>
2025-06-18 16:33:56 -0500

    fix: rename costs to items and setup mock data

--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <BudgetHeader :budget="budget" />
-    <BudgetTable :costs="budget.items" />
+    <BudgetTable :items="budget.items" />
   </div>
 </template>
 
@@ -15,6 +15,20 @@ const budget = {
   needs: 123,
   wants: 123,
   future: 123,
-  items: [{}, {}],
+  items: [
+    {
+      name: "Paycheck",
+      type: "income",
+      dueOn: "",
+      frequency: "payPeriod",
+    },
+    {
+      name: "Paycheck 2",
+      type: "income",
+      amount: 150,
+      dueOn: "1st of month",
+      frequency: "biannually",
+    },
+  ],
 }
 </script>

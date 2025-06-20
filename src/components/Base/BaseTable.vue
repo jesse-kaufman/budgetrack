185a355e11d3038e332b88c2af1cd50f2ba12704
jesse-kaufman <jesse@jessekaufman.com>
2025-06-20 16:43:57 -0500

    style: tweak table width

--- src/components/Base/BaseTable.vue
+++ src/components/Base/BaseTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-2 pb-4 overflow-x-auto scroll-container">
     <table
-      class="min-w-full border border-separate rounded-lg shadow-lg border-gray-800/80 border-spacing-0"
+      class="w-full max-w-4xl border border-separate rounded-lg shadow-lg border-gray-800/80 border-spacing-0"
     >
       <slot></slot>
     </table>

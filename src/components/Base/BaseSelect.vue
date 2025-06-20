dc9ba43c8dff113477e18353dcd9b4075c0e9d49
jkaufman <jkaufman@peoples.bank>
2025-06-20 13:24:02 -0500

    feat: add BaseSelect as reusable select component

---
+++ src/components/Base/BaseSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <template v-if="!loading">
+    <select v-model="modelValue" class="w-full text-sm">
+      <option disabled>-- Select One --</option>
+      <option
+        v-for="option in normalizedOptions"
+        :key="option.value ?? option"
+        :value="option.value ?? option"
+        :selected="modelValue === option.value"
+      >
+        {{ option.label ?? option }}
+      </option>
+    </select>
+  </template>
+  <template v-else>
+    <div class="skeleton"></div>
+  </template>
+</template>
+
+<script setup>
+import { computed } from "vue"
+const modelValue = defineModel({ type: String })
+
+const { options } = defineProps({
+  loading: {
+    type: Boolean,
+    default: true,
+  },
+  options: {
+    type: [Array, Object],
+    required: true,
+  },
+})
+
+/** Stores normalized options, converting object options to an array for looping. */
+const normalizedOptions = computed(() => {
+  if (Array.isArray(options)) return options
+  return Object.entries(options).map(([value, label]) => ({ value, label }))
+})
+</script>

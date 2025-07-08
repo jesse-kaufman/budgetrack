a773b856d4f95f836847be5f5dec3e3df44f90a9
jesse-kaufman <jesse@jessekaufman.com>
2025-07-07 20:35:41 -0500

    chore: make table auto-width instead of full width

--- src/components/Base/BaseTable.vue
+++ src/components/Base/BaseTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-2 pb-4 overflow-x-auto scroll-container">
     <table
-      class="w-full max-w-4xl border border-separate rounded-lg shadow-lg border-gray-800/80 border-spacing-0"
+      class="max-w-4xl rounded-lg border shadow-lg border-separate border-gray-800/80 border-spacing-0"
     >
       <slot></slot>
     </table>

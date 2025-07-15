0e98b3c31fb17ce369dc9c13c753be01351109e9
jesse-kaufman <jesse@jessekaufman.com>
2025-07-14 19:57:22 -0500

    fix: remove x-axis padding from BaseTable container

--- src/components/Base/BaseTable.vue
+++ src/components/Base/BaseTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-x-auto px-2 pb-4 scroll-container">
+  <div class="scroll-container pb-4 overflow-x-auto">
     <table
       class="max-w-4xl rounded-lg border shadow-lg border-separate border-gray-800/80 border-spacing-0"
     >

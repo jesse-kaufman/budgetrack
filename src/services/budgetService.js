20900c9e3a168288e34c19f254eadcc2e732a00c
jesse-kaufman <jesse@jessekaufman.com>
2025-07-14 19:59:27 -0500

    fix: move budget mock data to db.json

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -6,110 +6,19 @@ import { validBudgetItemTypes } from "@/config/budgetConfig"
  * Gets budget from repository.
  * @returns {object} Object containing budget.
  */
-// eslint-disable-next-line max-lines-per-function
 export const getBudget = async () => {
-  const budget = {
-    lastUpdated: new Date(),
-    items: [
-      {
-        name: "Paycheck",
-        type: "income",
-        amount: 2000,
-        frequency: "payPeriod",
-      },
-      {
-        name: "Mortgage",
-        type: "loanPayment",
-        amount: 565,
-        frequency: "monthly",
-      },
-      {
-        name: "Student Loan",
-        type: "loanPayment",
-        amount: 264,
-        frequency: "monthly",
-      },
-      {
-        name: "Electricity",
-        type: "bill",
-        amount: 216,
-        dueOn: "15th of month",
-        frequency: "monthly",
-      },
-      {
-        name: "Natural Gas",
-        type: "bill",
-        amount: 81,
-        dueOn: "7th of month",
-        frequency: "monthly",
-      },
-      {
-        name: "City Utilities",
-        type: "bill",
-        amount: 130,
-        dueOn: "1st of month",
-        frequency: "monthly",
-      },
-      {
-        name: "Christmas savings",
-        type: "savingsTransfer",
-        amount: 10,
-        frequency: "payPeriod",
-      },
-      {
-        name: "Safety Net",
-        type: "investmentTransfer",
-        amount: 10,
-        frequency: "payPeriod",
-      },
-      {
-        name: "Car Insurance",
-        type: "bill",
-        amount: 250,
-        frequency: "semiannually",
-        dueOn: "May / Nov",
-      },
-      {
-        name: "Car Gas",
-        type: "variableExpense",
-        amount: 250,
-        frequency: "monthly",
-        dueOn: "",
-      },
-      {
-        name: "Credit Card",
-        type: "ccPayment",
-        amount: 250,
-        frequency: "monthly",
-        dueOn: "",
-      },
-      {
-        name: "Fast Food",
-        type: "takeout",
-        amount: 250,
-        frequency: "monthly",
-        dueOn: "",
-      },
-      {
-        name: "Shopping",
-        type: "shopping",
-        amount: 200,
-        frequency: "monthly",
-        dueOn: "",
-      },
-    ],
-  }
-
-  //  const res = await fetch(`${API_BASE}/budget`, {
-  //    credentials: "include",
-  //  })
-
-  // if (!res.ok) throw new Error("Failed to fetch budget")
-  // return res.json()
-
-  budget.items = sortItemsByTypeOrder(budget.items)
-
+  // eslint-disable-next-line no-magic-numbers
   await new Promise((r) => setTimeout(r, 1000))
+
+  // Get budget from API
+  const res = await fetch(`${API_BASE}/budget`)
+
+  // Throw error if request failed
+  if (!res.ok) throw new Error("Failed to fetch budget")
+
+  // Get budget from response and sort items by type
+  const budget = await res.json()
+  budget.items = sortItemsByTypeOrder(budget.items)
 
   return budget
 }

100e226207e3bffbeb60a16825117b7735274453
jesse-kaufman <jesse@jessekaufman.com>
2025-07-14 19:58:34 -0500

    feat: add view heading to app layout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="w-fit mx-auto">
+    <h1 class="text-2xl font-bold">{{ title }}</h1>
+    <h1 class="text-2xl font-bold">{{ title }}</h1>
     <slot />
   </div>
 </template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>

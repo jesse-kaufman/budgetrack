8bee8142e40bf16e290215b49f403a9fac7b718a
jesse-kaufman <jesse@jessekaufman.com>
2025-07-14 19:58:15 -0500

    style: format with headwind

--- src/components/Base/BaseTable.vue
+++ src/components/Base/BaseTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="scroll-container pb-4 overflow-x-auto">
     <table
-      class="max-w-4xl rounded-lg border shadow-lg border-separate border-gray-800/80 border-spacing-0"
+      class="border-gray-800/80 border-spacing-0 max-w-4xl border border-separate rounded-lg shadow-lg"
     >
       <slot></slot>
     </table>

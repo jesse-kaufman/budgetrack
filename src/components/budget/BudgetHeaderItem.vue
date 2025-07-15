6e4e522968731afd1e6bd0f499340171ab33f7fd
jesse-kaufman <jesse@jessekaufman.com>
2025-07-14 20:26:21 -0500

    style: format with headwind

--- src/components/budget/BudgetHeaderItem.vue
+++ src/components/budget/BudgetHeaderItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-4 py-2 min-w-1/3 sm:min-w-auto grow last:border-gray-800/80 last:border-b-1 last:border-r-1"
+    class="min-w-1/3 sm:min-w-auto grow last:border-gray-800/80 last:border-b-1 last:border-r-1 px-4 py-2"
   >
     <div class="text-[.8em] uppercase font-black">{{ name }}:</div>
     <div v-if="loading" class="mt-1.5 mb-1 skeleton min-w-10"></div>

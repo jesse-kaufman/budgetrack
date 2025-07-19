beb775605660033f1e5130c8ed16776fc41d575c
jesse-kaufman <jesse@jessekaufman.com>
2025-07-19 09:13:59 -0500

    fix: bind attrs to table and disable inheritAttrs

--- src/components/base/BaseTable.vue
+++ src/components/base/BaseTable.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="scroll-container pb-4 overflow-x-auto">
     <table
-      class="border-gray-800/80 border-spacing-0 max-w-4xl border border-separate rounded-lg shadow-lg"
+      v-bind="$attrs"
+      class="border-gray-800/80 border-spacing-0 border border-separate rounded-lg shadow-lg"
     >
       <slot></slot>
     </table>
   </div>
 </template>
+
+<script setup>
+defineOptions({ inheritAttrs: false })
+</script>

45bbd905978295ba18dfe1899f71623fad8891ef
jesse-kaufman <jesse@jessekaufman.com>
2025-08-09 14:02:14 -0500

    chore: removed unused files

--- backend/src/utils/textUtils.js
+++
@@ -1,8 +0,0 @@
-/** @file Text utility functions. */
-
-/**
- * Capitalizes the first letter of a string.
- * @param {string} str - String to format.
- * @returns {string} Formatted string.
- */
-export const toUpperFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1)

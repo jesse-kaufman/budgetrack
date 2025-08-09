5a5b1aade5ff20014fb8f9ae0f69ed305660fcb6
jesse-kaufman <jesse@jessekaufman.com>
2025-08-09 15:39:07 -0500

    chore: add import aliases for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 /** @file Vite configuration. */
+import path from "node:path"
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
@@ -10,6 +11,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "#base": path.resolve(__dirname, "./src/components/base"),
+      "#budget": path.resolve(__dirname, "./src/components/budget"),
+      "#config": path.resolve(__dirname, "./src/config"),
+      "#services": path.resolve(__dirname, "./src/services"),
+      "#stores": path.resolve(__dirname, "./src/stores"),
+      "#layouts": path.resolve(__dirname, "./src/layouts"),
+      "#ui": path.resolve(__dirname, "./src/components/ui"),
+      "#utils": path.resolve(__dirname, "./src/utils"),
+      "#views": path.resolve(__dirname, "./src/views"),
     },
   },
   server: {
@@ -20,7 +30,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (p) => p.replace(/^\/api/, ""),
       },
     },
   },

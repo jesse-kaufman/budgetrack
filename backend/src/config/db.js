5d419b7e96af39052adbcfe8668aae53356b8f54
jesse-kaufman <jesse@jessekaufman.com>
2025-08-09 15:58:37 -0500

    chore: add default strings for all config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,15 +3,15 @@
 /** Database type; defaults to mssql. */
 const type = process.env.DB_TYPE || "mssql"
 /** Database host. */
-const host = process.env.DB_HOST
+const host = process.env.DB_HOST || ""
 /** Database port; defaults to 1433. */
 const port = process.env.DB_PORT ? Number(process.env.DB_PORT) : 1433
 /** Database user. */
-const username = process.env.DB_USER
+const username = process.env.DB_USER || ""
 /** Database password. */
-const password = process.env.DB_PASS
+const password = process.env.DB_PASS || ""
 /** Database name. */
-const database = process.env.DB_NAME
+const database = process.env.DB_NAME || ""
 /** Whether to enable database logging. */
 const logging = process.env.DB_LOGGING === "true"
 /** Whether to enable TypeORM's database sync. */

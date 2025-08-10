deb09f033829e9778cd2509cc443ee10d5c17d83
jesse-kaufman <jesse@jessekaufman.com>
2025-08-09 21:47:04 -0500

    chore: remove unused files

--- backend/__tests__/unit/placeholder.test.js
+++
@@ -1,8 +0,0 @@
-/** @file Placeholder unit test. */
-import { describe, expect, it } from "vitest"
-
-describe("Placeholder", () => {
-  it("should be true", () => {
-    expect(true).toBe(true)
-  })
-})

85f273bacb0a1c545223e247de46f3ca990c1592
jesse-kaufman <jesse@jessekaufman.com>
2025-08-09 23:48:35 -0500

    db: make amount and scheduled not null and default scheduled to false

---
+++ backend/migrations/1754800353764-migration.js
@@ -0,0 +1,25 @@
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ */
+
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
+export class Migration1754800353764 {
+    name = 'Migration1754800353764'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE TABLE "temporary_budgetItems" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "type" varchar CHECK( "type" IN ('income','loanPayment','ccPayment','bill','variableExpense','shopping','takeout','entertainment','savings','investments') ) NOT NULL, "amount" decimal(18,2) NOT NULL, "frequency" varchar CHECK( "frequency" IN ('payPeriod','monthly','semiannually','yearly') ) NOT NULL, "scheduled" boolean NOT NULL DEFAULT (0), "budgetId" integer, CONSTRAINT "FK_6e324f5cad509df2602b04f2ed3" FOREIGN KEY ("budgetId") REFERENCES "budgets" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
+        await queryRunner.query(`INSERT INTO "temporary_budgetItems"("id", "name", "type", "amount", "frequency", "scheduled", "budgetId") SELECT "id", "name", "type", "amount", "frequency", "scheduled", "budgetId" FROM "budgetItems"`);
+        await queryRunner.query(`DROP TABLE "budgetItems"`);
+        await queryRunner.query(`ALTER TABLE "temporary_budgetItems" RENAME TO "budgetItems"`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "budgetItems" RENAME TO "temporary_budgetItems"`);
+        await queryRunner.query(`CREATE TABLE "budgetItems" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "type" varchar CHECK( "type" IN ('income','loanPayment','ccPayment','bill','variableExpense','shopping','takeout','entertainment','savings','investments') ) NOT NULL, "amount" decimal(18,2), "frequency" varchar CHECK( "frequency" IN ('payPeriod','monthly','semiannually','yearly') ) NOT NULL, "scheduled" boolean, "budgetId" integer, CONSTRAINT "FK_6e324f5cad509df2602b04f2ed3" FOREIGN KEY ("budgetId") REFERENCES "budgets" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
+        await queryRunner.query(`INSERT INTO "budgetItems"("id", "name", "type", "amount", "frequency", "scheduled", "budgetId") SELECT "id", "name", "type", "amount", "frequency", "scheduled", "budgetId" FROM "temporary_budgetItems"`);
+        await queryRunner.query(`DROP TABLE "temporary_budgetItems"`);
+    }
+}

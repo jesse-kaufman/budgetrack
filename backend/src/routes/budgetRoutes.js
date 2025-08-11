c297bf3c9d078c5b8975bf4615b695ef7c2f3469
jesse-kaufman <jesse@jessekaufman.com>
2025-08-11 06:26:14 -0500

    fix: clean up unused routes

--- backend/src/routes/budgetRoutes.js
+++ backend/src/routes/budgetRoutes.js
@@ -5,11 +5,6 @@ import BudgetController from "#controllers/BudgetController.js"
 const controller = new BudgetController()
 const projectRoutes = new Router()
 
-projectRoutes
-  .get("/", controller.getAll)
-  .get("/:id", controller.getById)
-  .post("/", controller.create)
-  .put("/:id", controller.update)
-  .delete("/:id", controller.delete)
+projectRoutes.get("/", controller.getAll).put("/", controller.update)
 
 export default projectRoutes

64537362f4de72e66761db3eb8aa1a9d9fd72bf6
jesse-kaufman <jesse@jessekaufman.com>
2025-08-11 06:27:17 -0500

    fix: adjust spacing in header

--- frontend/src/components/budget/BudgetHeader.vue
+++ frontend/src/components/budget/BudgetHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="flex flex-row flex-wrap mt-8 mb-4 align-items-start sm:flex-nowrap leading-4.5"
+    class="flex flex-row flex-wrap mt-8 mb-4 align-items-start sm:flex-nowrap leading-4.5 space-x-3"
   >
-    <div class="sm:w-fit w-full m-2">
+    <div class="sm:w-fit w-full my-2">
       <div class="ml-1 font-bold">Per Paycheck</div>
       <div
         class="rounded-xl divide-solid divide-gray-800/80 grow border-gray-800/50 flex flex-wrap overflow-hidden border border-collapse divide-x divide-y shadow-sm"
@@ -28,7 +28,7 @@
       </div>
     </div>
 
-    <div class="sm:w-fit w-full m-2">
+    <div class="sm:w-fit w-full my-2">
       <div class="ml-1 font-bold">Monthly</div>
       <div
         class="rounded-xl divide-solid divide-gray-800/80 grow border-gray-800/50 flex flex-wrap overflow-hidden border border-collapse divide-x divide-y shadow-sm"
@@ -54,7 +54,7 @@
       </div>
     </div>
 
-    <div class="sm:w-fit w-full m-2">
+    <div class="sm:w-fit w-full my-2">
       <div class="ml-1 font-bold">&nbsp;</div>
       <div
         class="rounded-xl divide-solid divide-gray-800/80 grow border-gray-800/50 flex flex-wrap overflow-hidden border border-collapse divide-x divide-y shadow-sm"
--- frontend/src/components/budget/BudgetHeaderItem.vue
+++ frontend/src/components/budget/BudgetHeaderItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="min-w-1/3 w-fit sm:min-w-auto grow last:border-gray-800/80 last:border-b-1 last:border-r-1 px-4 py-2"
+    class="min-w-1/3 w-fit sm:min-w-auto grow last:border-gray-800/80 last:border-b-1 last:border-r-1 px-4 py-3"
   >
     <div class="text-[.8em] uppercase font-black">{{ name }}:</div>
     <div v-if="loading" class="mt-1.5 mb-1 skeleton min-w-10"></div>
-    <div v-else>
+    <div class="pt-1 text-lg" v-else>
       <CurrencyField v-if="value" :model-value="value" :view-only="true" />
       <div v-else><slot /></div>
     </div>
